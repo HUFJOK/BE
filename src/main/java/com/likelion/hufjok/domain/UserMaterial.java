9566bb43542b6a3b027622e58f3008f34fbe7de7
Yebin0319 <kuyebin@naver.com>
2025-09-28 09:32:20 +0900

    create domain

--- src/main/java/com/likelion/hufjok/domain/UserMaterial.java
+++ src/main/java/com/likelion/hufjok/domain/UserMaterial.java
@@ -6,16 +6,16 @@ import jakarta.persistence.*;
 @Table(name = "user_material")
 public class UserMaterial {
 
+    // Many to Many 를 없애고자 만든 도메인
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id; // 복합키 대신 단순 기본키를 사용하는 것이 더 쉽습니다.
+    private Long id;
 
-    // 🌟 1. Material (자료) 와의 다대일 관계 (자료 ID를 외래키로 가짐)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "material_id", nullable = false)
     private Material material;
 
-    // 🌟 2. User (사용자) 와의 다대일 관계 (사용자 ID를 외래키로 가짐)
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
