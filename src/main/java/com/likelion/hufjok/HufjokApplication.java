fbdcef36d303f051f22192be62b7363a9afce1cd
Yebin0319 <kuyebin@naver.com>
2025-11-09 22:37:47 +0900

    배포 서버

--- src/main/java/com/likelion/hufjok/HufjokApplication.java
+++ src/main/java/com/likelion/hufjok/HufjokApplication.java
@@ -1,8 +1,15 @@
 package com.likelion.hufjok;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(
+		servers = {
+				@Server(url = "https://hufjok.lion.it.kr", description = "배포 서버")
+		}
+)
 @SpringBootApplication
 public class HufjokApplication {
 
