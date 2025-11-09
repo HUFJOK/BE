4c012c1350c232a0626bf2d6efdce3ddb6305917
Yebin0319 <kuyebin@naver.com>
2025-11-10 01:43:24 +0900

    SwaggerConfig 파일 업로드 문제 해결위해 추가

---
+++ src/main/java/com/likelion/hufjok/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.likelion.hufjok.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import org.springdoc.core.models.GroupedOpenApi;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.servers.Server;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("v1")
+                .pathsToMatch("/api/v1/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .addServersItem(new Server().url("https://hufjok.lion.it.kr")) // Swagger 요청 주소 지정
+                .components(new Components()
+                        .addSecuritySchemes("cookieAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.APIKEY)
+                                        .in(SecurityScheme.In.COOKIE)
+                                        .name("JSESSIONID"))); // ✅ 세션 쿠키로 인증
+    }
+}
