95c80deacbea213fed0668a8e293c31922b3f0fb
d2che <skyday0330@gmail.com>
2025-11-11 15:49:19 +0900

    작업 중인 내용 저장 - UserService saveAndFlush 적용

--- src/main/java/com/likelion/hufjok/HufjokApplication.java
+++ src/main/java/com/likelion/hufjok/HufjokApplication.java
@@ -6,15 +6,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @OpenAPIDefinition(
-		servers = {
-				@Server(url = "https://hufjok.lion.it.kr", description = "배포 서버")
-		}
+        servers = {
+                @Server(url = "https://hufjok.lion.it.kr", description = "배포 서버")
+        }
 )
 @SpringBootApplication
 public class HufjokApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HufjokApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HufjokApplication.class, args);
+    }
 
 }
